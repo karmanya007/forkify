293a2391727498d125d4979438a26a9d3794d063
Karmanya Veer Sharma <karmanyasharma5@gmail.com>
2020-06-25 21:11:02 +0530

    Babel and functionality changes

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+// Search : https://forkify-api.herokuapp.com/api/search
+// GET: https://forkify-api.herokuapp.com/api/get
+
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/* * Global state of the app
+   * - Search object
+   * - Current recipe object
+   * - Shopping list object
+   * - Liked recipes  
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+	// 1) Get query from the view
+	const query = searchView.getInput(); // TODO
+
+	if (query) {
+		// 2) New search object and add to state
+		state.search = new Search(query);
+
+		// 3) Prepare the UI for results
+		searchView.clearInput();
+		searchView.clearResults();
+		renderLoader(elements.searchRes);
+
+		// 4) Search for recipes
+		await state.search.getResults();
+
+		// 5) Render results on UI
+		clearLoader();
+		searchView.renderResults(state.search.result);
+	}
+};
+
+elements.searchForm.addEventListener('submit', (e) => {
+	e.preventDefault(); // To prevent the reloding of page after every submit action
+	controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', (e) => {
+	const btn = e.target.closest('.btn-inline');
+	if (btn) {
+		const goToPage = parseInt(btn.dataset.goto, 10);
+		searchView.clearResults();
+		searchView.renderResults(state.search.result, goToPage);
+		// console.log(goToPage);
+	}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,9 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry     : './src/js/index.js',
+	entry     : [
+		'./src/js/index.js'
+	],
 	output    : {
 		path     : path.resolve(__dirname, 'dist'),
 		filename : 'js/bundle.js'
@@ -15,5 +17,16 @@ module.exports = {
 			filename : 'index.html',
 			template : './src/index.html'
 		})
-	]
+	],
+	module    : {
+		rules : [
+			{
+				test    : /\.js$/,
+				exclude : /node_modules/,
+				use     : {
+					loader : 'babel-loader'
+				}
+			}
+		]
+	}
 };
